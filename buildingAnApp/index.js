11d72b030e70a9a6f8ac3f2df49f0d7ef8719b44
Sean Esteva <seangeleno@gmail.com>
2016-07-16 02:25:50 -0700

    added commentList map()

--- buildingAnApp/index.js
+++ buildingAnApp/index.js
@@ -10,30 +10,76 @@
 
 //myFirst component #comment
 
-class CommentComponent extends React.Component{
-  render(){
-    <div className="commentBox">
-      <h3>Comments</h3>
-      <h4 className="comment-count">2 Comments</h4>
-      <div className="comment-list">
-        <Comment author="jimbo" body="great body" />
-        <Comment author="jackson" body="i like beer" />
-      </div>
-    </div>
-  }
-}
-
-class Comment extends React.Component {
-  render(){
-    return (
-      <div className="comment">
-        <p className="comment-header">
-          {this.props.author}
-        </p>
-        <div className="comment-actions">
-          <a href="#">Delete comment</a>
+  class CommentComponent extends React.Component{
+    render(){
+      const comments = this._getComments();
+      return(
+      <div className="commentBox">
+        <h3>Comments</h3>
+        /*<h4 className="comment-count">There are this many ==> {comments.length} comments</h4> */
+        <h4 className="comment-count">
+        {this.getCommentsTitle(comments.length)}
+        </h4>
+        <div className="comment-list">
+           {comments} /*jsx renders arrays*/
         </div>
       </div>
     );
+
+    _getComments() {
+      const commentList = [
+        {id: 1, auhor: "Morgan Freeman", body: "Great stuff!"},
+        {id: 2, auhor: "John Morgan", body: "Terrible stuff!"}
+      ];
+
+      return commentList.map(( comment )=> {
+        return (
+          /*important to include key to keep track of sequential comments*/
+          <Comment
+          author={comment.author}
+          body={comment.body}
+          key={ comment.id }
+          />);
+      });
+    }
   }
-}
+
+  _getCommentsTitle(commentCount){
+      if (commentCount === 0 || !commentCount) {
+        return "No comments yet";
+      } else if (commentCount === 1 ) {
+        return '1 comment';
+      } else {
+        return '${commentCount} comments';
+      }
+  }
+
+
+  class Comment extends React.Component {
+    render(){
+      return (
+        <div className="comment">
+          <p className="comment-header">
+            {this.props.author}
+          </p>
+          <div className="comment-actions">
+            <a href="#">Delete comment</a>
+          </div>
+        </div>
+      );
+    }
+    _getComments() {
+      const commentList = [
+        {id: 1, auhor: "Morgan Freeman", body: "Great stuff!"},
+        {id: 2, auhor: "John Morgan", body: "Terrible stuff!"}
+      ];
+      /*map() method requires one argument, a function that accepts a single argument
+       representing the current item in the array.*/
+      return commentList.map(( comment )=> {
+        return (
+          /*important to include key to keep track of sequential comments*/
+          <Comment author={comment.author} body={comment.body} key={ comment.id } />
+        );
+      });
+    }
+  }
